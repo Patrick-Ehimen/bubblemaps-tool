d2ef15f095000a40756f033f0af73a71b59d4e7d
ayo <0xosepatrick@gmail.com>
2025-04-26 15:03:29 +0100

    ..

---
+++ public/index.ts
@@ -0,0 +1,23 @@
+import Arbitrum from "./assets/arbi-logo.png";
+import Avalanche from "./assets/avax-logo.png";
+import Ethereum from "./assets/eth-logo.png";
+import Fantom from "./assets/ftm-logo.png";
+import Solana from "./assets/sol-logo.png";
+import Polygon from "./assets/poly-logo.png";
+import Cronos from "./assets/cro-logo.png";
+import Base from "./assets/base-logo.png";
+import BSC from "./assets/bsc-logo.png";
+import Logo from "./logo.svg";
+
+export {
+  Arbitrum,
+  Avalanche,
+  Ethereum,
+  Fantom,
+  Solana,
+  Polygon,
+  Cronos,
+  Base,
+  BSC,
+  Logo,
+};
